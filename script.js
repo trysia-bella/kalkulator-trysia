a00860c2cbcb791ba4be2c005f06c87a6d3a4aa4
trysia-bella <157209494+trysia-bella@users.noreply.github.com>
2024-02-17 14:33:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Insert angka ke textview
+function insert (num){
+    document.form.textarea.value =document.form.textarea.value + num; 
+}
+
+// Fungsi hitung
+function equal(){
+    var hasil = document.form.textarea.value;
+    document.form.textarea.value = eval(hasil);
+}
+
+// clean
+function clean(){
+    document.form.textarea.value ="";
+}
+
+// delete
+function back(){
+    var hasil = document.form.textarea.value;
+    document.form.textarea.value = hasil .substring(0,hasil.length-1);
+
+}
